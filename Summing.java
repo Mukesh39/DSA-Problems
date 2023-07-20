5d3f4a48aba67452577d75e73beeb9071486a4e1
Mukesh Kumar <muk.786422@gmail.com>
2023-07-20 11:59:53 +0530

    Addition of Two Numbers

---
+++ Summing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Summing {
+
+
+    public static void main(String[] args)
+    {
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int sum = a+b ;
+        System.out.println("The sum of a and b is "+ sum);
+
+    }
+
+}
